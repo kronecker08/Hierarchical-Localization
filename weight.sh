269170a3f2b21b520100f79f0af172222c79662b
kronecker08 <km4943@nyu.edu>
2021-03-01 21:27:26 -0600

    add maskrcnn weights

--- weight.sh
+++ weight.sh
@@ -5,6 +5,10 @@ unzip vd16_pitts30k_conv5_3_vlad_preL2_intra_white.zip
 rm -r vd16_pitts30k_conv5_3_vlad_preL2_intra_white.zip
 
 cd ..
+cd Mask_RCNN
+wget https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5
+
+cd ..
 
 cd hfnet
 mkdir model
@@ -12,4 +16,5 @@ cd model
 wget http://robotics.ethz.ch/~asl-datasets/2019_CVPR_hierarchical_localization/hfnet_tf.tar.gz
 tar -xvzf hfnet_tf.tar.gz
 rm -r hfnet_tf.tar.gz
+
 
