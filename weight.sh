e373347fd1d401ea3aa0dc06132d7d3ba89802a9
kronecker08 <km4943@nyu.edu>
2021-03-01 20:45:11 -0600

    add

---
+++ weight.sh
@@ -0,0 +1,15 @@
+mkdir weights
+cd weights
+wget http://rpg.ifi.uzh.ch/datasets/netvlad/vd16_pitts30k_conv5_3_vlad_preL2_intra_white.zip
+unzip vd16_pitts30k_conv5_3_vlad_preL2_intra_white.zip
+rm -r vd16_pitts30k_conv5_3_vlad_preL2_intra_white.zip
+
+cd ..
+
+cd hfnet
+mkdir model
+cd model
+wget http://robotics.ethz.ch/~asl-datasets/2019_CVPR_hierarchical_localization/hfnet_tf.tar.gz
+tar -xvzf hfnet_tf.tar.gz
+rm -r hfnet_tf.tar.gz
+
